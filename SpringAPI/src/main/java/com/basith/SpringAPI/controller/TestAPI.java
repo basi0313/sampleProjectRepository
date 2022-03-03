492adab996d54c613a1c274a923918979b23a863
basit <basit@BASITH-ROG>
2022-03-03 21:47:37 +0530

    testing commit and push

---
+++ SpringAPI/src/main/java/com/basith/SpringAPI/controller/TestAPI.java
@@ -0,0 +1,16 @@
+package com.basith.SpringAPI.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class TestAPI {
+
+	@GetMapping("check")
+	public String check() {
+		return "working";
+	}
+	
+}
